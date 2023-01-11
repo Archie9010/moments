93062bc9e7669dd0a4ba2513e91353fe756cef7a
Archie9010 <archie9010@protonmail.com>
2023-01-11 12:25:35 +0000

    add commitment

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,3 +1,21 @@
-cosst baseURL = ""
+import { rest } from "msw"
 
-export const handlers = []
+const baseURL = "https://drf-api9010.herokuapp.com/"
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json({
+                pk: 1,
+                username: "admin",
+                email: "",
+                first_name: "",
+                last_name: "",
+                profile_id: 1,
+                profile_image: "https://res.cloudinary.com/dmdwnobji/image/upload/v1/media/../default_profile_qdjgyp"
+                })
+        );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200))
+    })
+];
